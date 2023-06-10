797c12191b53f8bda9e0480284a912f84f0720eb
Rodrogas666 <105886719+Rodrogas666@users.noreply.github.com>
2023-06-10 17:49:11 -0600

    primer commit xd

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'tarea0',
+        description: ' mi tarea0'
+    },
+    {
+        id: 1,
+        title: 'tarea1',
+        description: ' mi tarea1'
+    },
+    {
+        id: 2,
+        title: 'tarea2',
+        description: ' mi tarea2'
+    }
+];
